bc8b1bb106a2b8ec1918df9ad910030a3b3d0427
tobibias <tobibiasobiwanketobi@MacBook-Pro-5.local>
2026-09-23 12:04:27 +0200

    Fill prism universe with material elements

--- sustainable-reflecting-gallery.js
+++ sustainable-reflecting-gallery.js
@@ -15,7 +15,7 @@ const galleries = {
 const pieces = {
   cd: ["cd/element-01.png", "cd/element-06.png", "cd/element-09.png"],
   mirror: ["mirror/element-01.png", "mirror/element-03.png", "mirror/element-05.png"],
-  prism: ["prism/element-02.png"],
+  prism: ["prism/element-02.png", "prism/element-01.png", "prism/element-03.png"],
   random: ["random/element-01.png", "random/element-02.png"],
 };
 
